f69c5e94ee8ad1e57e431a8478472eb9ae017d49
Andrey <57635897+dfcz652@users.noreply.github.com>
2021-02-05 17:16:19 +0200

    Add files via upload

---
+++ 12WorsPerDay/src/sample/Controller.java
@@ -0,0 +1,150 @@
+package sample;
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+
+public class Controller {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Pane P1Start;
+
+    @FXML
+    private Label L1;
+
+    @FXML
+    private Label L2;
+
+    @FXML
+    private AnchorPane AP1Start;
+
+    @FXML
+    private AnchorPane MainPane;
+
+    @FXML
+    private  AnchorPane MondayPane;
+
+    @FXML
+    private  AnchorPane  TuesdayPane;
+
+    @FXML
+    private  AnchorPane  WednesdayPane;
+
+    @FXML
+    private Label L1Process;
+
+    @FXML
+    private Label L1StartDayMachine;
+
+    @FXML
+    private Label L2StartTimeMachine;
+
+    @FXML
+    private AnchorPane APProsess;
+
+    @FXML
+    private Button StartButton;
+
+    @FXML
+    private Button BTest1;
+
+    @FXML
+    void MPressed(MouseEvent event) {
+        StartButton.setStyle("-fx-background-color: #091533;");
+    }
+
+    @FXML
+    void MReleased(MouseEvent event) {
+        StartButton.setStyle("-fx-background-color: #2E3348;");
+    }
+
+    @FXML
+    void MEntered(MouseEvent event) {
+        StartButton.setStyle("-fx-background-color: #091533;");
+    }
+
+    @FXML
+    void MExited(MouseEvent event) {
+        StartButton.setStyle("-fx-background-color: #2E3348;");
+    }
+
+    @FXML
+    void initialize() {
+        L1StartDayMachine.setText(KonvertDayMachine(TakeDayMashine()));
+        L2StartTimeMachine.setText(TakeTimeMashine());
+        //StartButton.setOnAction(event -> OpenThatWords());
+    }
+
+    public String TakeDayMashine(){
+        Date currentDate = new Date();
+        SimpleDateFormat DayFormat = null;
+        DayFormat = new SimpleDateFormat("EEEE");
+        String DayFormat1 = DayFormat.format( currentDate );
+        //String BigDayFormat = DayFormat1.toUpperCase();
+        return DayFormat1;
+
+    }
+
+    public String TakeTimeMashine(){
+        Date currentDate = new Date();
+        SimpleDateFormat TimeFormat = null;
+        TimeFormat = new SimpleDateFormat("k:mm");
+        String TimeFormat1 = TimeFormat.format( currentDate );
+
+        return TimeFormat1;
+
+    }
+
+    public String KonvertDayMachine(String aStr) { //Универсальный метод с входящим параметром который делает первую букву входящего параметра Верхнего регистра
+        String FirstSymbol = aStr.substring(0,1);
+        String BigSymbol = FirstSymbol.toUpperCase();
+        String Res = aStr.replaceFirst(FirstSymbol,BigSymbol);
+
+        return Res;
+    }
+
+    public void OpenThatWords(){
+        String tDay;
+
+        tDay  = TakeDayMashine();
+
+        if(tDay.equals("понедельник")){
+            MondayPane.setVisible(true);
+            MainPane.setVisible(false);
+        }
+
+        if(tDay.equals("вторник")){
+            TuesdayPane.setVisible(true);
+            MainPane.setVisible(false);
+        }
+
+        if(tDay.equals("среда")){
+            WednesdayPane.setVisible(true);
+            MainPane.setVisible(false);
+        }
+
+//        if(TakeDayMashine() == "ЧЕТВЕРГ"){
+//            MondayPane.setVisible(true);
+//            MainPane.setVisible(false);
+//        }
+        else{
+            MainPane.setVisible(true);
+        }
+
+    }
+
+}
